e3169c69ea4d46552595d2ceb8ceeb0aec2a4f25
morrigangirl <morrigangirl@protonmail.com>
2025-05-21 09:38:39 -0400

    Bonfire

--- scripts/create-bonfire.js
+++ scripts/create-bonfire.js
@@ -1,5 +1,6 @@
 Hooks.on("midi-qol.preItemRollComplete", async (workflow) => {
   if (!workflow || !workflow.item) return;
+  console.log("Create Bonfire Hooked");
 
   // Match by name (case sensitive!)
   if (workflow.item.name === "Create Bonfire") {
