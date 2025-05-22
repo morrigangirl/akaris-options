91ceadea3f93e7836a3f91ea622ea0d43c0d8ab2
morrigangirl <morrigangirl@protonmail.com>
2025-05-22 16:12:20 -0400

    Still hacking socketlib

--- scripts/create-bonfire.js
+++ scripts/create-bonfire.js
@@ -2,7 +2,7 @@ let socket;
 
 Hooks.once("socketlib.ready", () => {
 	socket = socketlib.registerModule("akaris-options");
-	socket.register("sockCreateBonfire", sockCreatebonfire);
+	socket.register("sockCreateBonfire", sockCreateBonfire);
 });
 
 function sockCreateBonfire(userName) {
